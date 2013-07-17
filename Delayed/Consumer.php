5e18445bbbb69b1449374642beb1f1d6f34f9239
NoiseByNorthwest <noisebynorthwest@gmail.com>
2013-07-17 13:43:22 +0200

    Delayed consumer: limit requeuing delay to 3600s (nsqd internal limit)

--- Delayed/Consumer.php
+++ Delayed/Consumer.php
@@ -25,7 +25,7 @@ class Consumer implements ConsumerInterface
         $message = Message::factory($payload);
         $remaining = $message->getScheduledAt() - time();
         if ($remaining > 0) {
-            throw new RequeueMessageException($remaining * 1000);
+            throw new RequeueMessageException(min(3600, $remaining) * 1000);
         }
         $target = $this->topicManager->getTopic($message->getTargetTopic());
         if (!$target) {
